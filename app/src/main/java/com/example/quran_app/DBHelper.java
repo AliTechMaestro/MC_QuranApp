97acb8dfaa8ce79803c6aafb78985389d1e7a9f5
aliabbas <bsef19a525@pucit.edu.pk>
2022-08-31 09:01:37 +0500

    Commit 4 (Search Funtionality from DataBase)

--- app/src/main/java/com/example/quran_app/DBHelper.java
+++ app/src/main/java/com/example/quran_app/DBHelper.java
@@ -1,156 +1,101 @@
 package com.example.quran_app;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-public class DBHelper extends SQLiteOpenHelper{
+import androidx.annotation.Nullable;
 
-    public static final String AYA_ID = "AyaID";
-//    public static final String ARABIC_TEXT = "ArabicText";
-//    public static final String FATEH_TRANS = "FatehMuhammadJalandhri";
-//    public static final String MOHSIN_TRANS = "DrMohsinKhan";
+import java.util.ArrayList;
 
-    // database Path
-    private static String DB_PATH = "src/main/assets/databases/";
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String AyaID = "AyaID";
+    public static final String SurahID = "SuraID";
+    public static final String AyaNo = "AyaNo";
+    public static final String RakuID = "RakuID";
+    public static final String PRakuID = "PRakuID";
+    public static final String ParaID = "ParaID";
 
-    // database Name
-    private static final String DATABASE_NAME = "quran.sqlite";
+    public static final String ArabicText = "ArabicText";
+    public static final String FatehMuhammadJalandhri = "FatehMuhammadJalandhri";
+    public static final String MehmoodulHassan = "MehmoodulHassan";
+    public static final String DrMohsinKhan = "DrMohsinKhan";
+    public static final String MuftiTaqiUsmani = "MuftiTaqiUsmani";
+    public static final String Table_name="tayah";
 
-    // database Version
-    private static final int DATABASE_VERSION = 1;
+    public DBHelper(@Nullable Context context) {
+        super(context, "myquran.db", null, 1);
+    }
 
-    // Table Name of Database
-    static final String TABLE_NAME = "tayah";
-
-    public Context context;
-    static SQLiteDatabase sqliteDataBase;
-
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // String createTableSTatementOne = "CREATE TABLE CustTable(CustomerID Integer PRIMARY KEY AUTOINCREMENT, " + CUSTOMER_NAME_FIRST + " Text, CustomerAge Int, ActiveCustomer BOOL) ";
+        String createTableSTatement = "CREATE TABLE " + Table_name + "(" +
+                AyaID + " Integer PRIMARY KEY AUTOINCREMENT," + SurahID + " Integer,"
+                + AyaNo + " Integer, " + ArabicText + " String, "+FatehMuhammadJalandhri+" String," +
+                MehmoodulHassan+" String,"+DrMohsinKhan+" String,"+MuftiTaqiUsmani+" String," +
+                RakuID+" Integer,"+PRakuID+" Integer,"+ParaID+" Integer ) ";
+        db.execSQL(createTableSTatement);
+    }
 
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        // No need to write the create table query.
-        // As we are using Pre built data base.
-        // Which is ReadOnly.
-    }
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        // No need to write the update table query.
-        // As we are using Pre built data base.
-        // Which is ReadOnly.
-        // We should not update it as requirements of application.
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Table_name);
+        onCreate(db);
     }
 
+    public ArrayList<String> getUrduTranslation(int id) {
 
+        SQLiteDatabase db = this.getReadableDatabase();
 
-//    Constructor ---> takes reference of context to access application assets and resources
-
-    public DBHelper(Context context) {
-        super(context, DATABASE_NAME, null ,DATABASE_VERSION);
-        this.context = context;
-    }
-
-    /**  Creating an empty database on the system so that I can rewrite it with my own database.
-      By calling this method and empty database will be created into the default system path
-      of my application so we are gonna be able to overwrite that database with our database.
-      */
-    public void createDataBase() throws IOException{
-        //check if the database exists
-        boolean databaseExist = checkDataBase();
-
-        if(databaseExist){
-            // Do Nothing.
-        }else{
-            this.getWritableDatabase();
-            copyDataBase();
-        }// end if else dbExist
-    } // end createDataBase().
-
-    /** Checking whether the database already exist to avoid re-copying the file each time you open the application.
-     * @return true if it exists, false if it doesn't
-     */
-    public boolean checkDataBase(){
-        File databaseFile = new File(DB_PATH + DATABASE_NAME);
-        return databaseFile.exists();
-    }
-
-    /**
-     * Copies your database from your local assets-folder to the just created empty database in the
-     * system folder, from where it can be accessed and handled.
-     * This is done by transferring byte stream.
-     * */
-    private void copyDataBase() throws IOException {
-        //Open your local db as the input stream
-        InputStream myInput = context.getAssets().open(DATABASE_NAME);
-        // Path to the just created empty db
-        String outFileName = DB_PATH + DATABASE_NAME;
-        //Open the empty db as the output stream
-        OutputStream myOutput = new FileOutputStream(outFileName);
-        //transfer bytes from the input file to the output file
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = myInput.read(buffer)) > 0) {
-            myOutput.write(buffer, 0, length);
+        Cursor cursorCourses = db.rawQuery("SELECT "+MehmoodulHassan+" FROM " + Table_name+" WHERE "+AyaID+ " = "+ id ,null);
+        ArrayList<String> studentArrayList = new ArrayList<>();
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+                studentArrayList.add(cursorCourses.getString(0));
+            } while (cursorCourses.moveToNext());
         }
-    //Close the streams
-        myOutput.flush();
-        myOutput.close();
-        myInput.close();
+        cursorCourses.close();
+        return studentArrayList;
     }
+    public ArrayList<String> getEngTranslation(int id) {
 
+        SQLiteDatabase db = this.getReadableDatabase();
 
-    /**
-     * This method opens the data base connection.
-     * First it create the path up till data base of the device.
-     * Then create connection with data base.
-     */
-    public void openDataBase() throws SQLException{
-        //Open the database
-        String myPath = DB_PATH + DATABASE_NAME;
-        sqliteDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
-    }
-
-    /**
-     * This Method is used to close the data base connection.
-     */
-    @Override
-    public synchronized void close() {
-        if(sqliteDataBase != null)
-            sqliteDataBase.close();
-        super.close();
-    }
-    /**
-     * Apply your methods and class to fetch data using raw or queries on data base using
-     * following demo example code as:
-     */
-
-    public String getQuranData(String searchID) {
-        String result = "";
-        String query = "SELECT TABLE_NAME.ArabicText,TABLE_NAME.FatehMuhammadJalandhri,TABLE_NAME.DrMohsinKhan FROM "+TABLE_NAME+"WHERE "+AYA_ID+" ='"+searchID;
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        while (cursor.moveToNext()) {
-            String result_0 = cursor.getString(0);
-            String result_1 = cursor.getString(1);
-            String result_2 = cursor.getString(2);
-            result += String.valueOf(result_0) + " " + result_1 + System.getProperty("line.separator") + result_2;
+        Cursor cursorCourses = db.rawQuery("SELECT "+MuftiTaqiUsmani+" FROM " + Table_name+" WHERE "+AyaID+ " = "+ id ,null);
+        ArrayList<String> studentArrayList = new ArrayList<>();
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+                studentArrayList.add(cursorCourses.getString(0));
+            } while (cursorCourses.moveToNext());
         }
-        cursor.close();
-        db.close();
-        if(result.equals(""))
-            result="No Record Found";
-        return result;
+        cursorCourses.close();
+        return studentArrayList;
+    }
+
+    public ArrayList<String> search(int id) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursorCourses = db.rawQuery("SELECT "+ArabicText+" FROM " + Table_name+" WHERE "+AyaID+ " = "+ id ,null);
+//        Log.d("query executed",cursorCourses.getString(0));
+        ArrayList<String> quranArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+                quranArrayList.add(cursorCourses.getString(0));
+            } while (cursorCourses.moveToNext());
+
+        }
+        cursorCourses.close();
+        return quranArrayList;
     }
 
 }
-
--- app/src/main/java/com/example/quran_app/Search.java
+++ app/src/main/java/com/example/quran_app/Search.java
@@ -1,27 +1,32 @@
 package com.example.quran_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Search extends AppCompatActivity {
-
-    EditText editText;
-    Button searchBtn;
-    TextView result;
-
-    ListView listView;
+    Button urduTrans,engTrans,searchBtn;
+    ListView listViewAyat,listViewTrans,engListView;
+    EditText searchValue;
 
 
     @Override
@@ -29,22 +34,28 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        editText = findViewById(R.id.searchVal);
-        searchBtn = findViewById(R.id.searchBtn);
-
+        searchBtn = findViewById(R.id.btnSearch);
+        searchValue = findViewById(R.id.searchVal);
+        listViewAyat = findViewById(R.id.ayatListView);
+        listViewTrans = findViewById(R.id.TransListView);
+        engListView = findViewById(R.id.TransEngListView);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                DBHelper dbHelper = new DBHelper(Search.this);
-                String list = dbHelper.getQuranData(editText.getText().toString());
-                ArrayAdapter arrayAdapter = new ArrayAdapter<QuranModel>
-                        (Search.this,R.layout.quran_text, Integer.parseInt(list));
-                listView = findViewById(R.id.searchListView);
-                listView.setAdapter(arrayAdapter);
+            public void onClick(View view) {
 
+                DBHelper dbHelper = new DBHelper(Search.this);
+                Integer text=Integer.parseInt(searchValue.getText().toString());
+                List<String> ayat = dbHelper.search(text);
+                List<String> verseTranslation = dbHelper.getUrduTranslation(text);
+                List<String> engTrans = dbHelper.getEngTranslation(text);
+                ArrayAdapter verse = new ArrayAdapter<String>(Search.this, R.layout.quran_text,ayat);
+                ArrayAdapter translation = new ArrayAdapter<String>(Search.this,R.layout.list_item_text,verseTranslation);
+                ArrayAdapter engTranslation = new ArrayAdapter<String>(Search.this,R.layout.list_item_text,engTrans);
+                listViewAyat.setAdapter(verse);
+                listViewTrans.setAdapter(translation);
+                engListView.setAdapter(engTranslation);
             }
         });
-
     }
 }
